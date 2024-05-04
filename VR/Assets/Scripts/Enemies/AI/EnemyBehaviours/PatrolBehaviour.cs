9ab6d2009cfaa1ce6794dd8e5d7782bfc204ef67
Rihards Fabriks <swizls12@gmail.com>
2024-05-05 00:46:26 +0300

    Фикс просчёт пути врагов через закрытые двери

--- VR/Assets/Scripts/Enemies/AI/EnemyBehaviours/PatrolBehaviour.cs
+++ VR/Assets/Scripts/Enemies/AI/EnemyBehaviours/PatrolBehaviour.cs
@@ -1,5 +1,8 @@
-using UnityEngine;
 using EnemyAI.Utilites;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
 
 namespace EnemyAI 
 { 
@@ -7,7 +10,7 @@ namespace EnemyAI
     {
         private const string ANIMATION_NAME = "Walking";
 
-        private Vector3 _targetWaypoint;
+        private Waypoint _targetWaypoint;
         public PatrolBehaviour(EnemyBehaviourHandler enemyReference) : base(enemyReference)
         {
             _animationName = ANIMATION_NAME;
@@ -17,17 +20,14 @@ namespace EnemyAI
         public override void Enter()
         {
             _animationName = ANIMATION_NAME;
-            _targetWaypoint = GetNextWaypoint();
-            MoveToWaypoint();
-
             _enemyReference.EnemyMover.StartMoving();
         }
-    
+
         public override void Update()
         {
-            if (!EnemyBehaviourUtilities.CheckIsDestinationReached(_enemyReference, _targetWaypoint))
+            if (_enemyReference.EnemyMover.Agent.remainingDistance > 1f)
                 return;
-                
+
             _targetWaypoint = GetNextWaypoint();
             MoveToWaypoint();
         }
@@ -37,13 +37,30 @@ namespace EnemyAI
             _enemyReference.EnemyMover.StopMoving();
         }
 
-        private Vector3 GetNextWaypoint()
+        private Waypoint GetNextWaypoint()
         {
-            return _enemyReference.WaypointCointainer.Waypoints[Random.Range(0, _enemyReference.WaypointCointainer.Waypoints.Length)].transform.position;
+            List<Waypoint> waypoints = _enemyReference.WaypointCointainer.Waypoints.ToList();
+            int waypointsCount = waypoints.Count;
+            Waypoint waypoint = null;
+
+            for (int i = 0; i < waypointsCount; i++)
+            {
+                waypoint = waypoints[Random.Range(0, waypoints.Count - 1)];
+                NavMeshPath path = new NavMeshPath();
+                _enemyReference.EnemyMover.Agent.CalculatePath(waypoint.transform.position, path);
+
+                if (path.status == NavMeshPathStatus.PathComplete)
+                    return waypoint;
+                else
+                    waypoints.Remove(waypoint);
+            }
+
+            return null;
         }
+
         private void MoveToWaypoint()
         {
-            _enemyReference.EnemyMover.Agent.SetDestination(_targetWaypoint);
+            _enemyReference.EnemyMover.Agent.SetDestination(_targetWaypoint.transform.position);
         }
     }
 }
