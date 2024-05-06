65815c5f4d1d2af6c62b5b90b09894d5f8d354dc
Rihards Fabriks <swizls12@gmail.com>
2024-05-06 18:50:27 +0300

    Добавление модели ключ-карты и смена цвета в зависимости от типа

--- VR/Assets/Scripts/Interactables/Items/Keycards/Keycard.cs
+++ VR/Assets/Scripts/Interactables/Items/Keycards/Keycard.cs
@@ -1,9 +1,49 @@
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class Keycard : MonoBehaviour
 {
 
     [SerializeField] private KeycardType _keycardType;
 
+    [SerializeField] private int _targetMaterialIndex;
+
+    [Header("COLORS")]
+    [SerializeField] private Color _purple;
+    [SerializeField] private Color _red;
+    [SerializeField] private Color _blue;
+
+    private MeshRenderer _meshRenderer;
+
     public KeycardType Keycardtype => _keycardType;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        SetColor();
+    }
+
+    private void SetColor()
+    {
+        switch(_keycardType) 
+        {
+            case KeycardType.Purple:
+            {
+                _meshRenderer.materials[_targetMaterialIndex].color = _purple;
+                break;
+            }
+            case KeycardType.Red:
+            {
+                _meshRenderer.materials[_targetMaterialIndex].color = _red;
+                break;
+            }
+            case KeycardType.Blue:
+            {
+                _meshRenderer.materials[_targetMaterialIndex].color = _blue;
+                break;
+            }
+
+        }
+    }
 }
